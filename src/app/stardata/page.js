d1846262f5d03a03ec06c01c2b837097a552e262
Paul Stearns <celestialcoyote@gmail.com>
2023-08-17 18:18:16 -0700

    add mongodb

---
+++ src/app/stardata/page.js
@@ -0,0 +1,22 @@
+import { getStarData } from "@/api/mongo/stardata";
+
+
+const handler = async (req, res) => {
+	if (req.method === 'GET') {
+		try {
+			const { stardata, error } = await getStarData();
+
+			if (error) throw new Error(error);
+
+			return res.status(200).json({ stardata });
+		} catch (error) {
+			return  res.status(500).json({ error: error.message });
+		}
+	}
+
+	//res.setHeader('Allow', ['GET']);
+	//res.status(425).end(`Method ${req.method} is not allowed.`);
+}
+
+
+export default handler;
