f6b6637621b73dc11058764ef9c5ee7673291d38
Paul Stearns <celestialcoyote@gmail.com>
2023-08-30 18:07:18 -0700

    working on navbar

--- src/app/components/NavBar/page.js
+++ src/app/components/NavBar/page.js
@@ -1,35 +1,62 @@
-import React from "react";
 import Link from "next/link";
 
 
-const Navbar = () => {
+export default function Navbar() {
+
+	const navigation = [
+		{
+			name: "Home",
+			href: "/",
+			//icon: UserIcon
+		},
+		{
+			name: "Astronomy DB",
+			href: "/astronomy",
+			//icon: UsersIcon
+		},
+	];
 
 	return (
-		<div className="w-full h-20 bg-emerald-800 sticky top-0">
-			<div className="container mx-auto px-4 h-full">
-				<div className="flex justify-between items-center h-full">
-					<ul className="hidden md:flex gap-x-6 text-white">
-						<li>
-							<Link href="/about">
-								<p>About</p>
-							</Link>
-						</li>
-						<li>
-							<Link href="/stardata">
-								<p>Stardata</p>
-							</Link>
-						</li>
-						<li>
-							<Link href="/marvel">
-								<p>Marvel</p>
-							</Link>
-						</li>
-					</ul>
-				</div>
+		<div className="w-full h-20 bg-purple-400 sticky top-0">
+
+			<div className="flex bg-slate-400">
+				{navigation.map((item) => {
+					// <Link
+					// 	key={item.name}
+					// 	href={item.href}
+					// >
+					<p className="text-white bg-black">{item.name}</p>
+					// </Link>
+				})}
 			</div>
+
+
+
+
+			{/* <div className="container mx-auto px-4 h-full border-4">
+				<div className="flex justify-between items-center h-full border-4 border-yellow-500">
+					<div>
+						<Link href="/">
+							<p>Home</p>
+						</Link>
+					</div>
+					<div>
+						<Link href="/stardata">
+							<p>Astronomy DB</p>
+						</Link>
+					</div>
+					<div>
+						<Link href="/marvel">
+							<p>Marvel</p>
+						</Link>
+					</div>
+					<div>
+						<Link href="/about">
+							<p>About</p>
+						</Link>
+					</div>
+				</div>
+			</div> */}
 		</div>
 	);
 };
-
-
-export default Navbar;
