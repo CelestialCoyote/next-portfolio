ae44bd528117b7fd9427c60ca324d976dad9d62b
Paul Stearns <celestialcoyote@gmail.com>
2023-08-30 19:42:47 -0700

    update to navbar

--- src/app/components/NavBar/page.js
+++ src/app/components/NavBar/page.js
@@ -14,49 +14,26 @@ export default function Navbar() {
 			href: "/astronomy",
 			//icon: UsersIcon
 		},
+		{
+			name: "Marvel API",
+			href: "/marvel",
+			//icon: UsersIcon
+		},
 	];
 
 	return (
-		<div className="w-full h-20 bg-purple-400 sticky top-0">
-
-			<div className="flex bg-slate-400">
-				{navigation.map((item) => {
-					// <Link
-					// 	key={item.name}
-					// 	href={item.href}
-					// >
-					<p className="text-white bg-black">{item.name}</p>
-					// </Link>
-				})}
+		<div className="w-full h-20 bg-purple-500 sticky top-0">
+			<div className="container mx-auto flex items-center h-full">
+				{navigation.map((item) => (
+					<Link
+						key={item.name}
+						href={item.href}
+						className="pl-10 text-2xl text-blue-300"
+					>
+						{item.name}
+					</Link>
+				))}
 			</div>
-
-
-
-
-			{/* <div className="container mx-auto px-4 h-full border-4">
-				<div className="flex justify-between items-center h-full border-4 border-yellow-500">
-					<div>
-						<Link href="/">
-							<p>Home</p>
-						</Link>
-					</div>
-					<div>
-						<Link href="/stardata">
-							<p>Astronomy DB</p>
-						</Link>
-					</div>
-					<div>
-						<Link href="/marvel">
-							<p>Marvel</p>
-						</Link>
-					</div>
-					<div>
-						<Link href="/about">
-							<p>About</p>
-						</Link>
-					</div>
-				</div>
-			</div> */}
 		</div>
 	);
 };
