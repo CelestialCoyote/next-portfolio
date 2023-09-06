374d49b8497cd7a49a953e733b6954964f64a98b
Paul Stearns <celestialcoyote@gmail.com>
2023-09-05 17:16:28 -0700

    added nasa apod

---
+++ src/app/nasa/page.jsx
@@ -0,0 +1,40 @@
+import Link from 'next/link';
+
+
+export default function Nasa() {
+
+	return (
+		<div className='flex flex-col m-4'>
+
+			<div className='flex flex-col items-center mt-10'>
+				<h1 className='text-6xl mb-10'>NASA API</h1>
+
+				<div className="flex flex-col items-center mb-8">
+					<Link
+						className="text-2xl mb-6 text-cyan-500"
+						href="/nasa/apod"
+					>
+						Astronomy Photo of the Day
+					</Link>
+
+					<div className="w-2/3">
+						<p>
+							One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of the most popular websites across all federal agencies.
+						</p>
+					</div>
+				</div>
+
+				<div className="flex flex-col">
+					<Link
+						className="text-2xl mb-6 text-cyan-500"
+						href="/image-video-library"
+					>
+						Image and Video Library
+					</Link>
+				</div>
+
+			</div>
+
+		</div>
+	);
+};
