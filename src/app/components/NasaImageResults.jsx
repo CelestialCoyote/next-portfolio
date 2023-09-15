43a53e74c0875bc164e8f5efad7fa2f0c456ed9c
Paul Stearns <celestialcoyote@gmail.com>
2023-09-14 20:15:48 -0700

    image details mostly working

--- src/app/components/NasaImageResults.jsx
+++ src/app/components/NasaImageResults.jsx
@@ -27,8 +27,6 @@ export default function NasaImageResults({ items }) {
 					key={item.links[0].href}
 					className="flex flex-col items-center m-8"
 				>
-					{console.log(item)}
-					{/* {console.log(item.links)} */}
 					{/* <Link href={{ pathname: '/about', query: { data: JSON.stringify(episode) } }}></Link> */}
 					<Link
 						href={`/nasa/library/image/${item.data[0].nasa_id}`}
@@ -47,22 +45,11 @@ export default function NasaImageResults({ items }) {
 						</div>
 					</Link>
 
-					{/* {details &&
-						<div className="flex flex-col text-white mt-4">
-							<p>NASA ID: {item.data[0].nasa_id}</p>
-							<p>Title: {item.data[0].title}</p>
-							<p>Photographer: {item.data[0].photographer}</p>
-							<p>Location: {item.data[0].location}</p>
-							<p>Date Created: {item.data[0].date_created.slice(0, 10)}</p>
-							<p>Description: {item.data[0].description}</p>
-						</div>
-					} */}
-
 					<button
 						className="mt-6 border-red-300 border-2"
 						onClick={() => {
 							setDetails(true);
-							setImage(item.data[0]);
+							setImage(item);
 						}}
 					>
 						Details
@@ -71,16 +58,42 @@ export default function NasaImageResults({ items }) {
 			))}
 
 			{details &&
-				<div>
-					<p>NASA ID: {image.nasa_id}</p>
-					<button
-						className="mt-6 border-red-300 border-2"
-						onClick={() => {
-							setDetails(false);
-						}}
-					>
-						Back
-					</button>
+				<div className="flex flex-col p-8">
+					{/* {console.log(image)} */}
+					<div className="flex flex-row gap-8 border-2">
+						<div className="flex">
+							<Image
+								className="w-auto h-full"
+								src={image.links[0].href}
+								alt="thumbnail"
+								placeholder="blur"
+								blurDataURL={image.links[0].href}
+								width="0"
+								height="0"
+								sizes="100vh"
+								priority={true}
+							/>
+						</div>
+						<div className="flex flex-col text-white mt-4 w-1/3">
+							<p>NASA ID: {image.data[0].nasa_id}</p>
+							<p>Title: {image.data[0].title}</p>
+							<p>Photographer: {image.data[0].photographer}</p>
+							<p>Location: {image.data[0].location}</p>
+							<p>Date Created: {image.data[0].date_created.slice(0, 10)}</p>
+							<p>Description: {image.data[0].description}</p>
+						</div>
+					</div>
+
+					<div>
+						<button
+							className="mt-10 border-red-300 border-2"
+							onClick={() => {
+								setDetails(false);
+							}}
+						>
+							Back
+						</button>
+					</div>
 				</div>
 			}
 
