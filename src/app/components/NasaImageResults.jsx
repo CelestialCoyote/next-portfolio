43a24254e51e8c2420c2f200c8a5fb1f973c43f9
Paul Stearns <celestialcoyote@gmail.com>
2023-09-18 20:10:53 -0700

    removing excess css

--- src/app/components/NasaImageResults.jsx
+++ src/app/components/NasaImageResults.jsx
@@ -25,12 +25,12 @@ export default function NasaImageResults({ items }) {
 					mb-6
 				"
 			>
-
 				{!details && items && items.map((item) => (
 					<div
 						className='
 							flex
 							flex-col
+							w-72
 							overflow-hidden
 							rounded-lg
 							border-purple-400
@@ -48,31 +48,34 @@ export default function NasaImageResults({ items }) {
 								width="0"
 								height="0"
 								sizes="100vh"
-								priority={true}
+								// priority={true}
 							/>
 						</div>
-						<div className='h-48 p-6 bg-black'>
-							<p className='text-sm font-medium text-cyan-700'>
-								{item.data[0].date_created.slice(0, 10)}
-							</p>
-							<div className='flex flex-col justify-between h-full'>
+						<div className='flex flex-col justify-between items-center h-48 p-4'>
+							<div>
+								<p className='text-sm font-medium text-cyan-700'>
+									{item.data[0].date_created.slice(0, 10)}
+								</p>
 								<p className='mt-2 line-clamp-3'>
 									{item.data[0].title}
 								</p>
-								<button
-									className="
-										lg:hover:underline
-										cursor-pointer
-										mb-4
-									"
-									onClick={() => {
-										setDetails(true);
-										setImage(item);
-									}}
-								>
-									DETAILS
-								</button>
 							</div>
+							<button
+								className="
+										w-18
+										rounded-lg
+										cursor-pointer
+										hover:underline
+										border-purple-500
+										border-2
+									"
+								onClick={() => {
+									setDetails(true);
+									setImage(item);
+								}}
+							>
+								DETAILS
+							</button>
 						</div>
 					</div>
 				))}
@@ -80,21 +83,35 @@ export default function NasaImageResults({ items }) {
 				{details &&
 
 					<div className='flex flex-col items-center justify-center'>
-						<div className='container rounded-lg lg:flex lg:bg-transparent'>
-							<div className='relative h-[50vh] mb-4 lg:mb-0 lg:h-auto lg:w-1/2 rounded-lg focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-0 focus-within:ring-indigo-400'>
-									<Image
-										className="w-auto h-full"
-										src={image.links[0].href}
-										alt="thumbnail"
-										placeholder="blur"
-										blurDataURL={image.links[0].href}
-										width="0"
-										height="0"
-										sizes="100vh"
-										priority={true}
-									/>
+						<div className="container rounded-lg">
+							<div
+								className="
+									relative
+									h-[50vh]
+									mb-4
+									lg:mb-0
+									lg:h-auto
+									lg:w-1/2
+									rounded-lg
+									focus-within:outline-none
+									focus-within:ring-2
+									focus-within:ring-offset-0
+									focus-within:ring-indigo-400
+								"
+							>
+								<Image
+									className="w-auto h-full"
+									src={image.links[0].href}
+									alt="thumbnail"
+									placeholder="blur"
+									blurDataURL={image.links[0].href}
+									width="0"
+									height="0"
+									sizes="100vh"
+									// priority={true}
+								/>
 							</div>
-							
+
 							<div className='p-4 overflow-hidden bg-black lg:bg-transparent lg:pt-0 lg:w-1/2'>
 								<div>
 									<p className='text-2xl font-bold'>
