ac927eff41f9446363219eac2ef9dc8466d51b52
Paul Stearns <celestialcoyote@gmail.com>
2023-10-05 14:09:22 -0700

    react map gl popups working

--- src/app/components/Map/MeteorSitesMap.jsx
+++ src/app/components/Map/MeteorSitesMap.jsx
@@ -10,18 +10,18 @@ import meteors from '@/app/data/meteor.json'
 export default function MeteorSitesMap() {
 	const [meteorSite, setMeteorSite] = useState(null);
 
-	useEffect(() => {
-		const listener = e => {
-			if (e.key === "Escape") {
-				setMeteorSitek(null);
-			}
-		};
-		window.addEventListener("keydown", listener);
+	// useEffect(() => {
+	// 	const listener = e => {
+	// 		if (e.key === "Escape") {
+	// 			setMeteorSite(null);
+	// 		}
+	// 	};
+	// 	window.addEventListener("keydown", listener);
 
-		return () => {
-			window.removeEventListener("keydown", listener);
-		};
-	}, []);
+	// 	return () => {
+	// 		window.removeEventListener("keydown", listener);
+	// 	};
+	// }, []);
 
 	return (
 		<Map
@@ -40,22 +40,55 @@ export default function MeteorSitesMap() {
 					key={index}
 					latitude={meteor.geometry.coordinates[1]}
 					longitude={meteor.geometry.coordinates[0]}
+					onClick={e => {
+						// If we let the click event propagates to the map, it will immediately close the popup
+						// with `closeOnClick: true`
+						e.originalEvent.stopPropagation();
+						setMeteorSite(meteor);
+					}}
 				>
-					<button
-						className="bg-transparent cursor-pointer w-5 h-5"
+					<img
+						className="rounded-md w-5 h-5"
+						src="/asteroid.png"
+						alt="Asteroid Icon"
+					/>
+					{/* <button
+						className="bg-transparent cursor-pointer"
 						onClick={e => {
 							e.preventDefault();
 							setMeteorSite(meteor);
 						}}
 					>
-						<img src="/asteroid.png" alt="Asteroid Icon" />
-					</button>
+						<img
+							className="rounded-md w-5 h-5"
+							src="/asteroid.png"
+							alt="Asteroid Icon"
+						/>
+					</button> */}
 				</Marker>
 			))}
 
+			{/* {meteorSite ? (
+				//console.log(meteorSite.properties.crater_name)
+				//console.log(meteorSite.geometry.coordinates)
+
+				<Popup
+					latitude={meteorSite.geometry.coordinates[1]}
+					longitude={meteorSite.geometry.coordinates[0]}
+					onClose={() => {
+						setMeteorSite(null);
+						console.log('pupup closed');
+					}}
+				>
+					<div className="text-black">
+						{meteorSite.properties.crater_name}
+					</div>
+				</Popup>
+			) : null} */}
+
 			{meteorSite && (
 				//console.log(meteorSite.properties.crater_name)
-				//console.log(meteorSite.geometry.coordinates[0])
+				//console.log(meteorSite.geometry.coordinates)
 
 				<Popup
 					anchor="bottom"
@@ -63,10 +96,47 @@ export default function MeteorSitesMap() {
 					longitude={meteorSite.geometry.coordinates[0]}
 					onClose={() => {
 						setMeteorSite(null);
-						console.log('pupup closed')
+						console.log('pupup closed');
 					}}
 				>
-					{meteorSite.properties.crater_name}
+					<div className="flex flex-col text-black bg-slate-200">
+						<div className="flex">
+							<p className="font-bold mr-2">
+								Name:
+							</p>
+							{meteorSite.properties.crater_name}
+						</div>
+						<div className="flex">
+							<p className="font-bold mr-2">
+								State:
+							</p>
+							{meteorSite.properties.state}
+						</div>
+						<div className="flex">
+							<p className="font-bold mr-2">
+								Country:
+							</p>
+							{meteorSite.properties.country}
+						</div>
+						<div className="flex">
+							<p className="font-bold mr-2">
+								Target Rock:
+							</p>
+							{meteorSite.properties.target_rock}
+						</div>
+						<div className="flex">
+							<p className="font-bold mr-2">
+								Diameter (km):
+							</p>
+							{meteorSite.properties.diameter_km}
+						</div>
+						<div className="flex">
+							<p className="font-bold mr-2">
+								Age (millions years ago):
+							</p>
+							{meteorSite.properties.age_millions_years_ago}
+						</div>
+					</div>
 				</Popup>
 			)}
 		</Map>
