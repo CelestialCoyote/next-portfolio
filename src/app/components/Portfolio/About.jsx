0c14f86dcfc154ecd1d632d8cb88380cbc9f6559
Paul Stearns <celestialcoyote@gmail.com>
2023-10-09 20:03:58 -0700

    working on about

--- src/app/components/Portfolio/About.jsx
+++ src/app/components/Portfolio/About.jsx
@@ -6,50 +6,12 @@ import './About.css';
 
 const About = () => {
     return (
-        <section id="about">
-            <h5>Get to Know</h5>
-            <h2>About Me</h2>
-
-            <div className='about-container'>
-                <div className='about-me'>
-                    <div className='about-me-image'>
-                        {/* <img src={ArthurDentAbout} alt='About image' /> */}
-                    </div>
-                </div>
-
-                <div className='about-content'>
-                    <div className='about-cards'>
-                        <article className='about-card'>
-                            <BsAward className='about-icon' />
-                            <h5>Experience</h5>
-                            <small>3+ Years Working</small>
-                        </article>
-
-                        <article className='about-card'>
-                            <FiUsers className='about-icon' />
-                            <h5>Clients</h5>
-                            <small>100+ Worldwide</small>
-                        </article>
-
-                        <article className='about-card'>
-                            <RiFoldersLine className='about-icon' />
-                            <h5>Projects</h5>
-                            <small>10+ Completed</small>
-                        </article>
-                    </div>
-
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                        laboris nisi ut aliquip ex ea commodo consequat.
-                    </p>
-
-                    <a href='#contact' className='btn btn-primary'>Let's Talk</a>
-                </div>
-
-            </div>
-
-        </section>
+        <div>
+			<div className="flex flex-col items-center">
+				<label className="text-2xl">Paul Stearns</label>
+				<label>Full Stack Developer</label>
+			</div>
+		</div>
     );
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,99 @@
-import About from "./components/Portfolio/About";
+import { BsPatchCheckFill } from 'react-icons/bs';
 
 
 export default function Home() {
 	return (
 		<div className="flex flex-col h-[calc(100vh-5rem)] items-center">
-			<label
-				className="text-3xl text-center mt-10"
-			>
-				Main Page
-			</label>
 
-			<About />
-			
+			<div className="flex flex-col items-center mt-10 w-full border-2">
+				<label className="text-3xl mb-8">Paul Stearns</label>
+				<label className="">Full Stack Developer</label>
+			</div>
+
+			<div className="w-full text-center border-2">
+				<label>Skills</label>
+
+				<div className="flex w-full">
+
+					<div className="flex flex-col w-1/2 border-2">
+						<h3>Frontend Development</h3>
+						<div className="grid grid-cols-2 gap-4">
+							<div className="flex gap-2 p-2">
+								<BsPatchCheckFill className='experience-details-icon' />
+								<div>
+									<h4>HTML</h4>
+									<small className='text-light'>Experienced</small>
+								</div>
+							</div>
+							<div className="flex gap-2 p-2">
+								<BsPatchCheckFill className='experience-details-icon' />
+								<div>
+									<h4>CSS</h4>
+									<small className='text-light'>Experienced</small>
+								</div>
+							</div>
+							<div className="flex gap-2 p-2">
+								<BsPatchCheckFill className='experience-details-icon' />
+								<div>
+									<h4>JavaScript</h4>
+									<small className='text-light'>Experienced</small>
+								</div>
+							</div>
+							<div className="flex gap-2 p-2">
+								<BsPatchCheckFill className='experience-details-icon' />
+								<div>
+									<h4>Bootstrap</h4>
+									<small className='text-light'>Experienced</small>
+								</div>
+							</div>
+							<div className="flex gap-2 p-2">
+								<BsPatchCheckFill className='experience-details-icon' />
+								<div>
+									<h4>jQuery</h4>
+									<small className='text-light'>Experienced</small>
+								</div>
+							</div>
+							<div className="flex gap-2 p-2">
+								<BsPatchCheckFill className='experience-details-icon' />
+								<div>
+									<h4>React</h4>
+									<small className='text-light'>Experienced</small>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div className="flex flex-col w-1/2 border-2">
+						<h3>Backend Development</h3>
+						<div className="grid grid-cols-2 gap-4">
+							<div className="flex gap-2 p-2">
+								<BsPatchCheckFill className='experience-details-icon' />
+								<div>
+									<h4>Node JS</h4>
+									<small className='text-light'>Experienced</small>
+								</div>
+							</div>
+							<div className="flex gap-2 p-2">
+								<BsPatchCheckFill className='experience-details-icon' />
+								<div>
+									<h4>MongoDB</h4>
+									<small className='text-light'>Experienced</small>
+								</div>
+							</div>
+							<div className="flex gap-2 p-2">
+								<BsPatchCheckFill className='experience-details-icon' />
+								<div>
+									<h4>mySQL</h4>
+									<small className='text-light'>Experienced</small>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+
 		</div>
 	);
 };
