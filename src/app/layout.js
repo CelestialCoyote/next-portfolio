70d6827ec174352f394fc102b68fd4d9162aab4e
Paul Stearns <celestialcoyote@gmail.com>
2023-11-12 15:47:26 -0800

    tried new navbars

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,7 @@
 
 import './globals.css';
-//import { Inter } from 'next/font/google';
 import Navbar from './components/NavBar';
 
-
-//const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
 	title: 'CelestialCoyote',
@@ -14,7 +11,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			{/* <body className={inter.className}> */}
 			<body>
 				<Navbar />
 				{children}
